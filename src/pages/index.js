d6e8e3c9290473997ea798be897460e17b991483
Ethan O'Brien <ethanjfobrien@gmail.com>
2018-09-02 18:11:40 +0100

    initial commit

---
+++ src/pages/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+const IndexPage = () => (
+  <div>
+    <h1>Hi people</h1>
+    <p>my name is Ethan, I am a computer engineer and edtech entreprenuer</p>
+    <Link to="/page-2/">Go to page 2</Link>
+  </div>
+)
+
+export default IndexPage
